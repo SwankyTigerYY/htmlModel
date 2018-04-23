dd54d734aa261ef066c32dfe425f2339b1964fa8
swankytiger <861970869@qq.com>
2018-04-23 21:39:34 +0800

    222

--- application/index/controller/News.php
+++ application/index/controller/News.php
@@ -11,5 +11,32 @@ namespace app\index\controller;
 use think\Controller;
 
 class News extends Controller{
+    public function view_index(){
+        return $this->fetch();
+    }
+    public function maintenance_index(){
+    return $this->fetch();
+}
 
+    public function add(){
+        return $this->fetch('offcanvas');
+    }
+
+    public function edit(){
+        $info = [
+            'zhuti' => '主题',
+            'faburen' => '发布人',
+        ];
+        $time = [
+            'begin_time' => time(),
+            'end_time' => time()
+        ];
+        $this->assign('info', $info);
+        $this->assign('time', $time);
+        return $this->fetch('offcanvas');
+    }
+
+    public function save(){
+        $this->success('添加成功');
+    }
 }
